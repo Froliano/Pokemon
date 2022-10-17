4a63bbde1c8f97e4a8d3e786e3d0d90f3f8f09bd
Froliano <froliano.yt@gmail.com>
2022-10-17 14:22:45 +0200

    world

--- assets/map/house.tsx
+++ assets/map/house.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="house" tilewidth="16" tileheight="16" tilecount="775" columns="31">
- <image source="../../../perso/game#2/assets/house.png" trans="ffffff" width="500" height="400"/>
+<tileset version="1.9" tiledversion="1.9.2" name="house" tilewidth="16" tileheight="16" tilecount="775" columns="31">
+ <image source="../sprites/house.png" trans="ffffff" width="500" height="400"/>
 </tileset>
